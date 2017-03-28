6b9dd9f2837526d62cb4cf8dc8b92b3ca1373fdb
Arnaud Jeansen <arnaud.jeansen@gmail.com>
2017-03-28 11:30:55 +0200

    Note that the ionic native's push and ionic cloud's push are different. So we shouldn't look at http://ionicframework.com/docs/v2/native/push/ at all. All we do should be based on https://docs.ionic.io/services/push/
    Inject the Push plugin and use it as per the documentation. This gives an annoying error in the browser but works all right on Android.
    All we needed to do was to create a push notification for the app on https://apps.ionic.io/app/27b80f26/push/overview

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Push, PushToken } from '@ionic/cloud-angular';
 
 import { NavController } from 'ionic-angular';
 
@@ -6,10 +8,23 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public push: Push) {
+
   }
 
+  ngOnInit() {
+    // Directly register the device when we render the home page
+    this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      console.log('Token saved:', t.token);
+    });
+
+    // Listen to push notifications
+    this.push.rx.notification().subscribe((msg) => {
+      alert(msg.title + ': ' + msg.text);
+    });
+  }
 }
