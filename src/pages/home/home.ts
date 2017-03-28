8644f5796d05425976e9df3d00c4217492aaa46d
Arnaud Jeansen <arnaud.jeansen@gmail.com>
2017-03-28 11:38:39 +0200

    Now that it works fine on Android, only run the push code there to avoid errors when working in the browser.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Push, PushToken } from '@ionic/cloud-angular';
 
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,21 +10,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController, public push: Push) {
+  constructor(public navCtrl: NavController, public platform: Platform, public push: Push) {
 
   }
 
   ngOnInit() {
-    // Directly register the device when we render the home page
-    this.push.register().then((t: PushToken) => {
-      return this.push.saveToken(t);
-    }).then((t: PushToken) => {
-      console.log('Token saved:', t.token);
-    });
+    // Push is not supported in the browser. So only activate it when on a real device (android for now)
+    if (this.platform.is('android')) {
+      // Directly register the device when we render the home page
+      this.push.register().then((t: PushToken) => {
+        return this.push.saveToken(t);
+      }).then((t: PushToken) => {
+        console.log('Token saved:', t.token);
+      });
 
-    // Listen to push notifications
-    this.push.rx.notification().subscribe((msg) => {
-      alert(msg.title + ': ' + msg.text);
-    });
+      // Listen to push notifications
+      this.push.rx.notification().subscribe((msg) => {
+        alert(msg.title + ': ' + msg.text);
+      });
+    } else {
+      console.log('No push notification for current target. Deploy to Android to test!!');
+    }
   }
 }
